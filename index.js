7face0af674dd04fab181ac75b1e493ab4e75a6f
meadch <meadch@gmail.com>
2016-09-07 17:51:43 -0700

    updates index

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
-var topFilms = [{
-           title: "Don't Breath",
-           tomatoMeter: 86,
-           boxOffice: 15.7
-       }, {
-           title: "Suicide Squad",
-           tomatoMeter: 26,
-           boxOffice: 10
-       }, {
-           title: "Pete's Dragon",
-           tomatoMeter: 87,
-           boxOffice: 6.5
-       }, {
-           title: "Kubo and the Two Strings",
-           tomatoMeter: 96,
-           boxOffice: 6.5
-       }, {
-           title: "Sausage Party",
-           tomatoMeter: 82,
-           boxOffice: 5.3
-       }];
+function objectCon(name,age,course, something){
+  return {
+    name: name,
+    age: age,
+    course: course,
+    something: something
+  }
+}
 
-function grabBoxOfficeData(arr){
-  return arr.map(function(value){
-    return "$" + value.boxOffice;
-  });
+var person = objectCon(
+  "Jeff",
+  28,
+  "MEAN",
+  heyThere
+);
+
+
+function heyThere(someCoolThing){
+  console.log(someCoolThing);
 }
